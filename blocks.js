a18f71330c684962d368637240523e4c01d28e88
pluswebsite <98026305+pluswebsite@users.noreply.github.com>
2023-12-12 22:14:25 +0100

    Add files via upload

---
+++ blocks.js
@@ -0,0 +1,4 @@
+var blocks = {}
+blocks.alert = {'com':'alert *p1*','par':1,'ui':'Alert *p1*'}
+blocks.setvar = {'com':'set *p1* *p2*', 'par':2,'ui':'Set *p1* value *p2*'}
+blocks.print = {'com':'debug *p1*','par':1,'ui':'Say *p1*'}
