a1c9074c485770df8377433df45d78655a6a46ed
Code Programer <codeprogramerpro@gmail.com>
2023-12-16 19:44:43 +0100

    To Main Project

--- blocks.js
+++ blocks.js
@@ -2,3 +2,4 @@ var blocks = {}
 blocks.alert = {'com':'alert *p1*','par':1,'ui':'Alert *p1*'}
 blocks.setvar = {'com':'set *p1* *p2*', 'par':2,'ui':'Set *p1* value *p2*'}
 blocks.print = {'com':'debug *p1*','par':1,'ui':'Say *p1*'}
+blocks.getJSONBin = {'com':'getJSONBin *p1* *p2*','par':2,'ui':'Get *p1* as *p2*','bgColor':'red','pl':['bin','key'],'func':'<a href="javascript:setFunc(nm.JSONBin)">JSONBin</a>'}
--- rs.js
+++ rs.js
@@ -1,3 +1,19 @@
 function set(v, q){
 window[v] = q;
 }
+var JSONBin = {}
+JSONBin.dt = {};
+function getJSONBin(bin, masterKey){
+	let req = new XMLHttpRequest();
+
+	req.onreadystatechange = () => {
+	  if (req.readyState == XMLHttpRequest.DONE) {
+	    JSONBin.dt = JSON.parse(req.responseText);
+	    runPart('JSONBin')
+	  }
+	};
+	
+	req.open("GET", "https://api.jsonbin.io/v3/b/" + bin + "/?meta=false", true);
+	req.setRequestHeader("X-Master-Key", masterKey);
+	req.send();
+}
