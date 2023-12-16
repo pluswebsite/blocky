2c743af437a450b09b31362d55acb74dd3a8770f
Code Programer <codeprogramerpro@gmail.com>
2023-12-16 20:26:31 +0100

    Say

--- rs.js
+++ rs.js
@@ -1,14 +1,13 @@
 function set(v, q){
 window[v] = q;
 }
-var JSONBin = {}
-JSONBin.dt = {};
+var JSONBinDT = {};
 function getJSONBin(bin, masterKey){
 	let req = new XMLHttpRequest();
 
 	req.onreadystatechange = () => {
 	  if (req.readyState == XMLHttpRequest.DONE) {
-	    JSONBin.dt = JSON.parse(req.responseText);
+	    JSONBinDT = JSON.parse(req.responseText);
 	    runPart('JSONBin')
 	  }
 	};
@@ -16,4 +15,27 @@ function getJSONBin(bin, masterKey){
 	req.open("GET", "https://api.jsonbin.io/v3/b/" + bin + "/?meta=false", true);
 	req.setRequestHeader("X-Master-Key", masterKey);
 	req.send();
+}
+function setJSONBin(va, vl, bin, masterKey){
+	JSONBinDT[va] = vl;
+	let req = new XMLHttpRequest();
+
+	req.onreadystatechange = () => {
+	  if (req.readyState == XMLHttpRequest.DONE) {
+	    console.log('JSONBin Update Request Fullfilled With Exit')
+	  }
+	};
+	
+	req.open("PUT", "https://api.jsonbin.io/v3/b/" + bin, true);
+	req.setRequestHeader("Content-Type", "application/json");
+	req.setRequestHeader("X-Master-Key", masterKey);
+	req.send(JSON.stringify(JSONBinDT));	
+}
+function debug(q){
+	if(typeof q == 'object'){
+		document.getElementById("console").innerHTML = document.getElementById("console").innerHTML + "<br>" + JSON.stringify(q);
+	}
+	else{
+		document.getElementById("console").innerHTML = document.getElementById("console").innerHTML + "<br>" + q;
+	}
 }
